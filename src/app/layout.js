7bb2f092f635c31c6328bbd6107c9c27e0d1a2b9
abbas <abbasghorbani779@gmail.com>
2024-09-11 14:27:04 +0330

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,16 @@
+
+import "./globals.css";
+
+
+export const metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
